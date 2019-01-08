5bb5a4609edc3e08f9edfab69ed18ed4e08637b1
João <mateusjoao22@gmail.com>
2019-01-08 14:29:52 -0200

    Resolvi o conflito de Olás

--- index.php
+++ index.php
@@ -1,11 +1,7 @@
 <?php
-<<<<<<< HEAD
+
 	echo "Olá, Mundo 456!";
 
-	echo "Olá, Udemy!";
-=======
-	echo "Olá, Mundo 123!";
+	echo "Olá, Udemy e olá Hcode!";
 
-	echo "Olá, Hcode!";
->>>>>>> 770464366a1c97f98ebdf89163a62313928c46f9
 ?>
